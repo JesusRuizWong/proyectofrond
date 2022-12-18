0c02c7a733d8f98e298ad8b6b20b8bc9c7566f99
JesusRuizWong <jesusruizwong@outlook.com>
2022-12-18 18:54:51 -0500

    af98

--- src/feactures/home/Home.jsx
+++ src/feactures/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Slider from './carousel/components';
-import Topcircuits from './topcircuits/topcircuits';
+import Topcircuits from './topcircuits/Topcircuits';
 import TopHotels from './tophotels/TopHotels';
 import Info from './info/info';
 

b077048cb0bdbfef65d098cda08f0fc706bb7331
JesusRuizWong <jesusruizwong@outlook.com>
2022-12-18 18:57:12 -0500

    vff

--- src/feactures/home/Home.jsx
+++ src/feactures/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Slider from './carousel/components';
 import Topcircuits from './topcircuits/Topcircuits';
 import TopHotels from './tophotels/TopHotels';
-import Info from './info/info';
+import Info from './info/Info';
 
 export default function Home() { 
   return (
